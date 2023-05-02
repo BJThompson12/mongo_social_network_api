554bb68c1ed894e672f9796ec76994e7366b8544
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-05-02 13:13:03 -0400

    created api routes structure and connected to DB

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const routes = require('./routes');
 const app = express();
-const controllers = require('./controllers');
+//const controllers = require('./controllers');
 
 const PORT = process.env.MONGODB_URL || 3001;
+
+mongoose.connect('mongodb://127.0.0.1:27017/social_network_api');
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to Database on Port: ${PORT} 🗄️`);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(`Error connecting to Database: ${err}`);
+});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on Port: ${PORT} 🚀`);
+});
