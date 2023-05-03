c1dd8af1c475099150a4f3ae7a7f7955382fea87
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-05-03 11:40:07 -0400

    completed User routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,14 @@ module.exports = {
   // testRoute(req, res) {
   //   console.log('i made it');
   // },
-  
+
+  //POST a new user
+  createUser(req, res) {
+  User.create(req.body)
+  .then((user) => res.json(user))
+  .catch((err) => res.status(500).json(err))
+  },
+
   // GET all Users
 getAllUsers(req, res){
 User.find()
@@ -16,14 +23,28 @@ User.find()
 .catch((err) => res.status(500).json(err))
 },
   // GET a single user by _id
-
-  //POST a new user
-createUser(req, res) {
-  User.create(req.body)
+getUserByID(req, res){
+  User.findById(req.params.id)
   .then((user) => res.json(user))
   .catch((err) => res.status(500).json(err))
-}
+},
   // PUT to update a user by its _id
-
+  updateUserByID(req, res){
+    User.findByIdAndUpdate(
+      {_id: req.params.id},
+      {$set: req.body},
+      {runValidators: true, new: true}
+      )
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err))
+  },
   // DELETE to remove a user byt its _id
+  deleteUserByID(req, res){
+    User.findByIdAndDelete(
+      {_id: req.params.id},
+      {runValidators: true, new: true}
+      )
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err))
+  },
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,10 +2,13 @@ const router = require('express').Router();
 
 // require the controller with the methods
 const {
-  getAllUsers, createUser
+  getAllUsers, createUser, getUserByID, updateUserByID, deleteUserByID
 } = require('../../controllers/userController')
 
 // /api/user
 router.route('/').get(getAllUsers).post(createUser);
+
+// api/user/:id
+router.route('/:id').get(getUserByID).put(updateUserByID).delete(deleteUserByID)
 
 module.exports = router;
